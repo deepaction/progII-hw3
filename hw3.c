aca62462932ffb0af5604e542f885e715ecc4493
Thodoris Lazarettos <tlazarettos@gmail.com>
2016-04-14 18:57:45 +0300

    Started developing hw3.c shell interface

--- hw3.c
+++ hw3.c
@@ -1,4 +1,7 @@
 #include <sys/types.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +10,7 @@
 #include <signal.h>
 
 #define ARGUMENT_NUMBER 20
+#define NAME_SIZE 512
 
 struct list
 {
@@ -191,27 +195,94 @@ int list_print()
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int i=0, rv, fd;
+	pid_t pid;
 	char *args[ARGUMENT_NUMBER];
+	char *command;
+	char *arguments;
+	char buf[NAME_SIZE];
 	
-	args[0]=strdup("./test");
-	for(i=1; i<5; i++)
-		args[i]=strdup("a");
-	
-	args[5]=NULL;
-		
+// 	args[0]=strdup("./test");
+// 	for(i=1; i<5; i++)
+// 		args[i]=strdup("a");
+// 	
+// 	args[5]=NULL;
+// 		
+// 	
+// 	struct list *node = create_list_entry( 12345, args,  0);
+// 	list_insert(node);
+// 	struct list *node1 = create_list_entry( 54355, args,  1);
+// 	list_insert(node1);
+// 	struct list *node2 = create_list_entry( 545, args,  0);
+// 	list_insert(node2);
+// 	node=list_search();
+// 	node=list_next();
+// 	list_delete(545);
+// 	
+// 	printf("%d\n", list_print());
+// 	
 	list_init();
-	struct list *node = create_list_entry( 12345, args,  0);
-	list_insert(node);
-	struct list *node1 = create_list_entry( 54355, args,  1);
-	list_insert(node1);
-	struct list *node2 = create_list_entry( 545, args,  0);
-	list_insert(node2);
-	node=list_search();
-	node=list_next();
-	//list_delete(545);
 	
-	printf("%d\n", list_print());
+	do
+	{
+		printf("$ ");
+		fgets(buf, NAME_SIZE, stdin);
+		command=strtok(buf, "  \n");
+		
+		if((strcmp(command, "quit")!=0)&&(strcmp(command, "info")!=0))
+		{
+			if(strcmp(command, "exec")==0)
+			{
+				arguments=command;
+				while(arguments!=NULL)
+				{
+					arguments=strtok(NULL, " ");
+					if(arguments==NULL)
+						break;
+					args[i]=strdup(arguments);
+					i++;
+				}
+				args[i-1][strlen(args[i-1])-1]='\0';
+				args[i]=NULL;
+				
+				pid=fork();
+					
+					if(pid==0)
+					{
+// 						fd=open("test.out", O_CREAT|O_TRUNC|O_WRONLY, 0766);
+// 						dup2(1, fd);
+						rv=execvp(args[0], args);
+						failcheck(rv,__LINE__-1);
+					}
+					printf("%d\n", pid);
+					list_insert(create_list_entry(pid, args,  0));
+			}
+			else
+			{
+				pid=atoi(strtok(NULL, " "));
+				if(strcmp(command, "term")==0)
+				{
+					;
+				}
+				else
+				{
+					;
+				}
+					
+			}
+		}
+		else if(strcmp(command, "info")==0)
+		{	
+			if(list_empty()!=0)
+				list_print();
+			else
+				printf("There are currently no active processes\n");
+		}	
+				
+		
+		//printf("%d\n", pid);
+		
+	}while(strcmp(command, "quit")!=0);
 	
 	free(head);
 	
